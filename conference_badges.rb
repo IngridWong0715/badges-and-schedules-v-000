4f8f8113db442b33c191eb5dbe7a95d453b1f7a3
Ingrid Wong <yw340@georgetown.edu>
2018-01-12 20:23:37 +0000

    automatically backed up by learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -5,7 +5,8 @@ end
 def batch_badge_creator(names)
   badge_messages=[]
   names.each do |name|
-    badge_messages.push(badge_maker(name))
+    message=badge_maker(name)
+    badge_messages.push(message)
   end
 end
 
